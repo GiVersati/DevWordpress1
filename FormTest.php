dc7e343a8374b77773fd7a49c08b824fef57c743
Gi <82105073+GiVersati@users.noreply.github.com>
2022-07-17 17:31:12 -0300

    Add files via upload

---
+++ FormTest.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Teste Dev Form
+ * Description:       Plugin para teste Dev
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Geovanna Versati
+ */
